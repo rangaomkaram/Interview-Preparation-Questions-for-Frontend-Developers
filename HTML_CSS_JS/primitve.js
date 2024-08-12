c6c9510eb44b056e120ee807b8fb6f4f737e9ca0
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-12 10:51:35 +0530

    adding primitive.js

---
+++ HTML_CSS_JS/primitve.js
@@ -0,0 +1,68 @@
+// Boolean 
+
+let isValid = true;
+console.log(isValid)
+
+let isPasswordValid = false;
+console.log(isPasswordValid)
+
+// Numbers
+
+let age = 31;
+
+let radius = 3.45;
+
+console.log(age);
+console.log(radius);
+
+
+// String
+
+let employeeName = "Ranga";
+let workingAs = 'Front-end Engineer';
+
+console.log(employeeName);
+console.log(workingAs)
+
+// Null
+
+let empty = null;
+console.log(empty);
+
+// Undefined 
+
+let getEmpty = undefined;
+console.log(getEmpty)
+
+//Symbol
+
+const sym = Symbol("foo");
+console.log(typeof sym); // "symbol"
+const symObj = Object(sym);
+console.log(typeof symObj); // "object"
+
+//BigInt 
+
+let x = BigInt(999999999999999);
+
+//print each type of varaibles
+
+console.log(typeof(age)); // Number
+console.log(typeof(radius));
+
+console.log(typeof isValid)//Boolean
+console.log(typeof(isPasswordValid));
+
+console.log(typeof employeeName);//String
+console.log(typeof(workingAs));
+
+console.log(typeof x);// BigInt
+
+console.log(typeof empty) //null
+
+console.log(typeof(getEmpty)) // undefined
+
+
+/* Here, we have seen primitive or object data types. */
+
+
