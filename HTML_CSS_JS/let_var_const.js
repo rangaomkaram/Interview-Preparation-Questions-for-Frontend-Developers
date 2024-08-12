345d3a47ea99ca9f81cf99288e7f6d8364c8ac8b
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-12 11:40:39 +0530

    concept of let var const

---
+++ HTML_CSS_JS/let_var_const.js
@@ -0,0 +1,42 @@
+
+// let
+
+let heroName = "NTR";
+console.log(heroName)
+
+heroName = "Jr.NTR";
+console.log(heroName);
+
+//const
+
+const PI = 3.1415;
+console.log(PI);
+// PI = 2.345;
+console.log(PI) // Type error : Assignment to const variable
+
+// 
+
+let age = 30
+
+age += 1;
+
+console.log(age)
+
+const birthYear = 1993;
+
+// birthYear = 1994;
+
+console.log(birthYear) // Type error : Assignment to constant variable.
+
+// get practice with some more examples
+
+let isFrontEndFun = true;
+console.log("Frontend is fun", isFrontEndFun);
+
+//Reassigning the variable later
+
+isFrontEndFun = "Yes";
+console.log("Is is Frontend still fun? ", isFrontEndFun);
+
+
+
