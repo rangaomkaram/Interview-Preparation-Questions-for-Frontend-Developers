ce5a9cc7ca134c84f631a50283ecf15abad2d400
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-14 11:36:37 +0530

    let's add some more operations in this file

--- HTML_CSS_JS/operator.js
+++ HTML_CSS_JS/operator.js
@@ -46,10 +46,31 @@ console.log(mySalary > yourSalary) // for comparision operators we print boolean
 console.log(mySalary < yourSalary)
 console.log(mySalary === yourSalary)
 
+// In comparsion operators === is a special case which checks type and value
+// In comparsion operators == only checks value
+
 // Type operators 
 
 console.log(typeof mySalary)
 console.log(typeof yourSalary)
+
+let empSalary = 1000;
+let hrSalary  = "1000";
+
+console.log(empSalary == hrSalary) // here this comparision operator only checks the value so it return true
+
+console.log(empSalary === hrSalary);// it returns false , because it checks the value and data type.
+
+// String Addition or concatenation
+
+let firstPerson = "udaykumar";
+let secondPerson = "Ranga"
+
+let concatenate = firstPerson + " " + secondPerson;
+let  secondConcatenate = secondPerson + " " + firstPerson;
+
+console.log(secondConcatenate)
+console.log(concatenate);
 
 
 
