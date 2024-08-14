b4aca4116319d2c4c676cc63cabb8e199e12aa6c
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-14 10:35:52 +0530

    operator.js is added

--- HTML_CSS_JS/operator.js
+++ HTML_CSS_JS/operator.js
@@ -5,3 +5,133 @@ let b = 3;
 let add = a + b;
 
 console.log(add);
+
+// Subtraction
+
+let sub = a - b;
+console.log(sub);
+
+//division 
+let div = a / b;
+console.log(div)
+
+//Multiplication 
+let mul = a * b;
+console.log(mul)
+
+
+//Similarly for square 
+
+let square = a ** b;
+console.log(square)
+
+// Remember BODMAS rule.
+
+// Modulus %
+
+let mod = a % b;
+console.log(mod);
+
+// incerment value 
+let number = 20;
+number = number + 1
+console.log(number)
+
+// Comparision Operators 
+let mySalary = 100;
+let yourSalary = 0;
+
+console.log(mySalary > yourSalary) // for comparision operators we print boolean value either true or false
+
+console.log(mySalary < yourSalary)
+console.log(mySalary === yourSalary)
+
+// Type operators 
+
+console.log(typeof mySalary)
+console.log(typeof yourSalary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
