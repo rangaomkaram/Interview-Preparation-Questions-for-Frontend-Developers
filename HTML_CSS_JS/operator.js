9e8569ca99fcd4c98357b02ca49ec5cc5e4662b5
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-15 07:29:33 +0530

    add concept of ternary operator

--- HTML_CSS_JS/operator.js
+++ HTML_CSS_JS/operator.js
@@ -57,9 +57,9 @@ console.log(typeof yourSalary)
 let empSalary = 1000;
 let hrSalary  = "1000";
 
-console.log(empSalary == hrSalary) // here this comparision operator only checks the value so it return true
+console.log(empSalary == hrSalary); // here this comparision operator only checks the value so it return true
 
-console.log(empSalary === hrSalary);// it returns false , because it checks the value and data type.
+console.log(empSalary === hrSalary); // it returns false , because it checks the value and data type.
 
 // String Addition or concatenation
 
@@ -71,6 +71,46 @@ let  secondConcatenate = secondPerson + " " + firstPerson;
 
 console.log(secondConcatenate)
 console.log(concatenate);
+
+
+//Logical Operators
+
+console.log(mySalary && yourSalary);
+console.log(mySalary || yourSalary);
+
+
+// Conditional Operators:(Ternart Operator)
+
+// This Ternart Operator ?
+
+/* This Ternart Operator in javascript, which is shorthand way of writing an if-else statement.
+
+It is used to return one value if the condition is true and another value if the condition is false
+
+The synatx for the ternary operator is as follows:
+
+condition ? exprIfTrue: exprIfFalse
+
+*/
+
+let isAdmin = true;
+let message = isAdmin ? "Hello Admin, you can access now" : "you are not Admin UnAuthorised to access";
+
+console.log(message)
+
+// This Assigning a value based on a condition
+
+let score = 90;
+let grade = score >=90 ? "You're grade is A+" : "You're grade is A";
+console.log(grade)
+
+// Using ternary operator with multiple conditions
+
+let age = 18;
+let result = age >= 18 ? "You're an adult " : age >=13 ? "You're a teenager" : "You're a kid";
+
+console.log(result)
+
 
 
 
