5084bf7c7eff5cbe14707d4b10588d8f4818dd22
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-20 21:12:45 +0530

    add

---
+++ Typescript/Basics/funda.js
@@ -0,0 +1,17 @@
+"use strict";
+/* let userName = "Namaste Ranga"
+
+let user = {
+    name : "Ranga",
+    occupation : "software developer"
+}
+
+console.log(
+    "Ranga Sesha"
+)
+
+console.log(user.occupation) */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Varaibles
+var message = "Hi, Ranga";
+console.log(message);
---
+++ Typescript/Basics/funda.ts
@@ -0,0 +1,20 @@
+/* let userName = "Namaste Ranga"
+
+let user = {
+    name : "Ranga",
+    occupation : "software developer"
+}
+
+console.log(
+    "Ranga Sesha"
+)
+
+console.log(user.occupation) */
+
+// Varaibles
+
+let message: string = "Hi, Ranga";
+
+console.log(message);
+
+export {}
